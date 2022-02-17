615456a2dc6b94728346d2fc5c2407ee58982119
fishkadealer229 <94692853+fishkadealer229@users.noreply.github.com>
2022-02-17 17:10:29 +0300

    mars image

---
+++ main.py
@@ -0,0 +1,41 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def name():
+    return "Миссия Колонизация Марса"
+
+
+@app.route('/index/')
+def index():
+    return 'И на Марсе будут яблони цвести!'
+
+
+@app.route('/promotion/')
+def promotion():
+    countdown_list = ['Человечество вырастает из детства.', 'Человечеству мала одна планета.',
+                      'Мы сделаем обитаемыми безжизненные пока планеты.', 'И начнем с Марса!',
+                      'Присоединяйся!']
+    return '</br>'.join(countdown_list)
+
+
+@app.route('/image_mars/')
+def mars_image():
+    return """<!doctype html>
+                    <html lang="en">
+                      <head>
+                        <meta charset="utf-8">
+                        <title>Привет, Марс!</title>
+                      </head>
+                      <body>
+                        <h1>Жди нас, Марс!</h1>
+                        <img src="/static/img/mars.jfif" alt="здесь должна была быть картинка, но не нашлась">
+                        <p>Это картинка луны</p>
+                      </body>
+                    </html>"""
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
